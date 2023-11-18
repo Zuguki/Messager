a183646efeb2f137d97805e1a15ec0f62c063f8e
Zakhar Guskov <guskov.zahar20@gmail.com>
2023-11-18 18:43:07 +0500

    Initial commit

---
+++ backend/Messager/Messager/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Messager.Controllers;
+
+[ApiController]
+[Route("api/v1")]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        return Ok(new[] {"123", "321"});
+    }
+}
