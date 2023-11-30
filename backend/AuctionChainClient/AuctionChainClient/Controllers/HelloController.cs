8e79deb03c582a7d2d386dd4f16557f9855f1466
Zakhar Guskov <guskov.zahar20@gmail.com>
2023-11-30 11:04:12 +0500

    messager domain models was added

---
+++ backend/AuctionChainClient/AuctionChainClient/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuctionChainClient.Controllers;
+
+[ApiController]
+[Route("hello")]
+public class HelloController : ControllerBase
+{
+    [HttpGet("auctions")]
+    public async Task<IActionResult> GetAuctions()
+    {
+        var client = new HttpClient();
+        var result = await client.GetAsync("http://localhost:5121/api/v1/auctions");
+        return Ok();
+    }
+}
